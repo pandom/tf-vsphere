a5b86d442d792302f774d8db1783df4ef0380b5e
pandom <pandom@users.noreply.github.com>
2022-08-16 12:12:58 +1000

    Fix Vars

--- variables.tf
+++ variables.tf
@@ -15,34 +15,34 @@ variable "vsphere_insecure" {
   default     = false
 }
 
-variable "vsphere_server" {
-    type = string
-    description = "vsphere server FQDN"
-    #default = "https://vcsa-98975.fe9dbbb3.asia-southeast1.gve.goog"
-}
+# variable "vsphere_server" {
+#     type = string
+#     description = "vsphere server FQDN"
+#     #default = "https://vcsa-98975.fe9dbbb3.asia-southeast1.gve.goog"
+# }
 
-variable "vsphere_password" {
-    type = string
-    description = "vsphere user/pass populated via a VAR_SET or fall back to ENVVAR"  
-}
+# variable "vsphere_password" {
+#     type = string
+#     description = "vsphere user/pass populated via a VAR_SET or fall back to ENVVAR"  
+# }
 
-variable "vsphere_user" {
-    type = string
-    description = "vsphere user/pass populated via a VAR_SET or fall back to ENVVAR"  
-}
+# variable "vsphere_user" {
+#     type = string
+#     description = "vsphere user/pass populated via a VAR_SET or fall back to ENVVAR"  
+# }
 
-variable "nsx_server" {
-    type = string
-    description = "NSX server FQDN"
-    #default = "https://nsx-98984.fe9dbbb3.asia-southeast1.gve.goog/"
-}
+# variable "nsx_server" {
+#     type = string
+#     description = "NSX server FQDN"
+#     #default = "https://nsx-98984.fe9dbbb3.asia-southeast1.gve.goog/"
+# }
 
-variable "nsx_user" {
-    type = string
-    description = "vsphere user/pass populated via a VAR_SET or fall back to ENVVAR"  
-}
+# variable "nsx_user" {
+#     type = string
+#     description = "vsphere user/pass populated via a VAR_SET or fall back to ENVVAR"  
+# }
 
-variable "nsx_password" {
-    type = string
-    description = "vsphere user/pass populated via a VAR_SET or fall back to ENVVAR"  
-}
+# variable "nsx_password" {
+#     type = string
+#     description = "vsphere user/pass populated via a VAR_SET or fall back to ENVVAR"  
+# }

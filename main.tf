ca843ce3b8721ec9bcc88b0e1524555aee0ef6db
pandom <pandom@users.noreply.github.com>
2022-08-16 11:19:10 +1000

    Changes

--- main.tf
+++ main.tf
@@ -9,8 +9,8 @@ terraform {
 }
 
 provider "vsphere" {
-  user                 = var.vsphere_user
-  password             = var.vsphere_password
-  vsphere_server       = var.vsphere_server
+  # user                 = var.vsphere_user
+  # password             = var.vsphere_password
+  # vsphere_server       = var.vsphere_server
   allow_unverified_ssl = var.allow_unverified_ssl
 }

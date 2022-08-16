390ff8f2b1d2c1a04c16afa2f6024c6b9fed5ea4
pandom <pandom@users.noreply.github.com>
2022-08-16 13:42:26 +1000

    Variables for NSX-SErver

--- variables.tf
+++ variables.tf
@@ -21,28 +21,8 @@ variable "vsphere_server" {
     #default = "https://vcsa-98975.fe9dbbb3.asia-southeast1.gve.goog"
 }
 
-# variable "vsphere_password" {
-#     type = string
-#     description = "vsphere user/pass populated via a VAR_SET or fall back to ENVVAR"  
-# }
-
-# variable "vsphere_user" {
-#     type = string
-#     description = "vsphere user/pass populated via a VAR_SET or fall back to ENVVAR"  
-# }
-
-# variable "nsx_server" {
-#     type = string
-#     description = "NSX server FQDN"
-#     #default = "https://nsx-98984.fe9dbbb3.asia-southeast1.gve.goog/"
-# }
-
-# variable "nsx_user" {
-#     type = string
-#     description = "vsphere user/pass populated via a VAR_SET or fall back to ENVVAR"  
-# }
-
-# variable "nsx_password" {
-#     type = string
-#     description = "vsphere user/pass populated via a VAR_SET or fall back to ENVVAR"  
-# }
+variable "nsx_server" {
+    type = string
+    description = "NSX server FQDN"
+    #default = "https://nsx-98984.fe9dbbb3.asia-southeast1.gve.goog/"
+}

10d7818722aa5869186dfa72194a1c4d123d5f11
pandom <pandom@users.noreply.github.com>
2022-09-12 11:55:27 +1000

    Add NSX

--- main.tf
+++ main.tf
@@ -12,6 +12,10 @@ terraform {
       version = "2.2.0"
       #ENVARS OR VARSETS for credentials
     }
+    nsxt = {
+      source = "vmware/nsxt"
+      version = "3.2.8"
+    }
   }
   
 }
@@ -21,4 +25,12 @@ provider "vsphere" {
   # password             = var.vsphere_password
   vsphere_server       = var.vsphere_server
   allow_unverified_ssl = var.allow_unverified_ssl
+}
+
+provider "nsxt" {
+  allow_unverified_ssl = var.allow_unverified_ssl
+  #host = var.nsx_server
+  max_retries = 3
+  username = var.nsx_user
+  
 }
